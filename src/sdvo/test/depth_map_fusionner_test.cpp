6862f754d156eb81a1fb3b05b72c9eedab98e5c7
David C <dav@dav.com>
2014-02-28 21:23:19 +0100

    Update Map Fusionner test

--- src/sdvo/test/depth_map_fusionner_test.cpp
+++ src/sdvo/test/depth_map_fusionner_test.cpp
@@ -20,33 +20,103 @@ TEST(depth_map_fusionner_test, opencvTests){
 
       cv::Mat1f M3 = M1_/M2_;
 }
-
-TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_observation_variance)
+TEST(depth_map_fusionner_test, depth_map_fusionner_test_nominal)
 {
-  float data_inverse_depth_prior[2] = {1,1};
-  float data_variance_prior[2] = {2,2};
+  float data_inverse_depth_prior[25] = {1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1};
+  float data_variance_prior[25] = {2,2,2,2,2,
+                                  2,2,2,2,2,
+                                  2,2,2,2,2,
+                                  2,2,2,2,2,
+                                  2,2,2,2,2};
+  float data_inverse_depth_obs[25] = {1,1,1,1,1,
+                                     1,1,1,1,1,
+                                     1,1,1,1,1,
+                                     1,1,1,1,1,
+                                     1,1,1,1,1};
+  float data_variance2_obs[25] = {2,2,2,2,2,
+                                 2,2,2,2,2,
+                                 2,2,2,2,2,
+                                 2,2,2,2,2,
+                                 2,2,2,2,2};
 
-  float data_inverse_depth_obs[2] = {1,1};
-  float data_variance2_obs[2] = {0,2};
-
-  cv::Mat1f inverse_depth_prior = cv::Mat1f(2,1,data_inverse_depth_prior);
-  cv::Mat1f variance_prior = cv::Mat1f(2,1,data_variance_prior);
-  cv::Mat1f inverse_depth_obs = cv::Mat1f(2,1,data_inverse_depth_obs);
-  cv::Mat1f variance_obs = cv::Mat1f(2,1,data_variance2_obs) ;
+  cv::Mat1f inverse_depth_prior = cv::Mat1f(5,5,data_inverse_depth_prior);
+  cv::Mat1f variance_prior = cv::Mat1f(5,5,data_variance_prior);
+  cv::Mat1f inverse_depth_obs = cv::Mat1f(5,5,data_inverse_depth_obs);
+  cv::Mat1f variance_obs = cv::Mat1f(5,5,data_variance2_obs) ;
 
   depth_ma_fusionner fusion(inverse_depth_obs,variance_obs,inverse_depth_prior,variance_prior);
   cv::Mat1f posterior = fusion.get_inverse_depth_posterior();
   cv::Mat1f variance = fusion.get_inverse_depth_posterior_variance();
-  std::cerr<<posterior<<std::endl;
-  std::cerr<<variance<<std::endl;
+  //std::cerr<<posterior<<std::endl;
+  //std::cerr<<variance<<std::endl;
 
-  EXPECT_EQ(posterior(0,0),1);
-  EXPECT_EQ(variance(0,0),2);
-  EXPECT_EQ(posterior(0,1),1);
-  EXPECT_EQ(variance(0,1),1);
+  for (int x = 0; x < 5; ++x) {
+    for (int y = 0; y < 5; ++y) {
+      if(x==2 && y==2){
+        EXPECT_TRUE(posterior(x,y)==1);
+        EXPECT_TRUE(variance(x,y)==1);
+      }
+      else{
+        EXPECT_TRUE(posterior(x,y)==1);
+        EXPECT_TRUE(variance(x,y)==1);
+      }
+    }
+  }
 }
 
-TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_observation_no_prior_variance)
+TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_obs_variance)
+{
+  float data_inverse_depth_prior[25] = {1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1};
+  float data_variance_prior[25] = {2,2,2,2,2,
+                                  2,2,2,2,2,
+                                  2,2,2,2,2,
+                                  2,2,2,2,2,
+                                  2,2,2,2,2};
+  float data_inverse_depth_obs[25] = {1,1,1,1,1,
+                                     1,1,1,1,1,
+                                     1,1,1,1,1,
+                                     1,1,1,1,1,
+                                     1,1,1,1,1};
+  float data_variance2_obs[25] = {2,2,2,2,2,
+                                 2,2,2,2,2,
+                                 2,2,0,2,2,
+                                 2,2,2,2,2,
+                                 2,2,2,2,2};
+
+  cv::Mat1f inverse_depth_prior = cv::Mat1f(5,5,data_inverse_depth_prior);
+  cv::Mat1f variance_prior = cv::Mat1f(5,5,data_variance_prior);
+  cv::Mat1f inverse_depth_obs = cv::Mat1f(5,5,data_inverse_depth_obs);
+  cv::Mat1f variance_obs = cv::Mat1f(5,5,data_variance2_obs) ;
+
+  depth_ma_fusionner fusion(inverse_depth_obs,variance_obs,inverse_depth_prior,variance_prior);
+  cv::Mat1f posterior = fusion.get_inverse_depth_posterior();
+  cv::Mat1f variance = fusion.get_inverse_depth_posterior_variance();
+  //std::cerr<<posterior<<std::endl;
+  //std::cerr<<variance<<std::endl;
+
+  for (int x = 0; x < 5; ++x) {
+    for (int y = 0; y < 5; ++y) {
+      if(x==2 && y==2){
+        EXPECT_TRUE(posterior(x,y)==1);
+        EXPECT_TRUE(variance(x,y)==2);
+      }
+      else{
+        EXPECT_TRUE(posterior(x,y)==1);
+        EXPECT_TRUE(variance(x,y)==1);
+      }
+    }
+  }
+}
+
+TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_prior_variance)
 {
   float data_inverse_depth_prior[25] = {1,1,1,1,1,
                                        1,1,1,1,1,
@@ -95,7 +165,7 @@ TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_observation_no_prior_
 
 }
 
-TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_observation_no_prior)
+TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_obs_data_no_prior_data)
 {
   float data_inverse_depth_prior[25] = {1,1,1,1,1,
                                        1,1,1,1,1,
@@ -143,7 +213,53 @@ TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_observation_no_prior)
   }
 }
 
-TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_observation)
+TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_obs_variance_no_prior_variance)
+{
+  float data_inverse_depth_prior[25] = {1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1};
+  float data_variance_prior[25] = {2,2,2,2,2,
+                                  2,2,2,2,2,
+                                  2,2,0,2,2,
+                                  2,2,2,2,2,
+                                  2,2,2,2,2};
+  float data_inverse_depth_obs[25] = {1,1,1,1,1,
+                                     1,1,1,1,1,
+                                     1,1,1,1,1,
+                                     1,1,1,1,1,
+                                     1,1,1,1,1};
+  float data_variance2_obs[25] = {2,2,2,2,2,
+                                 2,2,2,2,2,
+                                 2,2,0,2,2,
+                                 2,2,2,2,2,
+                                 2,2,2,2,2};
+
+  cv::Mat1f inverse_depth_prior = cv::Mat1f(5,5,data_inverse_depth_prior);
+  cv::Mat1f variance_prior = cv::Mat1f(5,5,data_variance_prior);
+  cv::Mat1f inverse_depth_obs = cv::Mat1f(5,5,data_inverse_depth_obs);
+  cv::Mat1f variance_obs = cv::Mat1f(5,5,data_variance2_obs) ;
+
+  depth_ma_fusionner fusion(inverse_depth_obs,variance_obs,inverse_depth_prior,variance_prior);
+  cv::Mat1f posterior = fusion.get_inverse_depth_posterior();
+  cv::Mat1f variance = fusion.get_inverse_depth_posterior_variance();
+
+  for (int x = 0; x < 5; ++x) {
+    for (int y = 0; y < 5; ++y) {
+      if(x==2 && y==2){
+        EXPECT_EQ(posterior(x,y),0);
+        EXPECT_EQ(variance(x,y),0);
+      }
+      else{
+        EXPECT_EQ(posterior(x,y),1);
+        EXPECT_EQ(variance(x,y),1);
+      }
+    }
+  }
+}
+
+TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_obs_data)
 {
   float data_inverse_depth_prior[25] = {1,1,1,1,1,
                                        1,1,1,1,1,
@@ -191,7 +307,55 @@ TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_observation)
   }
 }
 
-TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_prior)
+TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_obs_data_no_obs_variance)
+{
+  float data_inverse_depth_prior[25] = {1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1};
+  float data_variance_prior[25] = {2,2,2,2,2,
+                                  2,2,2,2,2,
+                                  2,2,2,2,2,
+                                  2,2,2,2,2,
+                                  2,2,2,2,2};
+  float data_inverse_depth_obs[25] = {1,1,1,1,1,
+                                     1,1,1,1,1,
+                                     1,1,0,1,1,
+                                     1,1,1,1,1,
+                                     1,1,1,1,1};
+  float data_variance2_obs[25] = {2,2,2,2,2,
+                                 2,2,2,2,2,
+                                 2,2,0,2,2,
+                                 2,2,2,2,2,
+                                 2,2,2,2,2};
+
+  cv::Mat1f inverse_depth_prior = cv::Mat1f(5,5,data_inverse_depth_prior);
+  cv::Mat1f variance_prior = cv::Mat1f(5,5,data_variance_prior);
+  cv::Mat1f inverse_depth_obs = cv::Mat1f(5,5,data_inverse_depth_obs);
+  cv::Mat1f variance_obs = cv::Mat1f(5,5,data_variance2_obs) ;
+
+  depth_ma_fusionner fusion(inverse_depth_obs,variance_obs,inverse_depth_prior,variance_prior);
+  cv::Mat1f posterior = fusion.get_inverse_depth_posterior();
+  cv::Mat1f variance = fusion.get_inverse_depth_posterior_variance();
+  //std::cerr<<posterior<<std::endl;
+  //std::cerr<<variance<<std::endl;
+
+  for (int x = 0; x < 5; ++x) {
+    for (int y = 0; y < 5; ++y) {
+      if(x==2 && y==2){
+        EXPECT_EQ(posterior(x,y),1);
+        EXPECT_EQ(variance(x,y),2);
+      }
+      else{
+        EXPECT_EQ(posterior(x,y),1);
+        EXPECT_EQ(variance(x,y),1);
+      }
+    }
+  }
+}
+
+TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_prior_data)
 {
   float data_inverse_depth_prior[25] = {1,1,1,1,1,
                                        1,1,1,1,1,
@@ -238,6 +402,145 @@ TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_prior)
     }
   }
 }
+
+TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_data_no_variance)
+{
+  float data_inverse_depth_prior[25] = {1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,0,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1};
+  float data_variance_prior[25] = {2,2,2,2,2,
+                                  2,2,2,2,2,
+                                  2,2,0,2,2,
+                                  2,2,2,2,2,
+                                  2,2,2,2,2};
+  float data_inverse_depth_obs[25] = {1,1,1,1,1,
+                                     1,1,1,1,1,
+                                     1,1,0,1,1,
+                                     1,1,1,1,1,
+                                     1,1,1,1,1};
+  float data_variance2_obs[25] = {2,2,2,2,2,
+                                 2,2,2,2,2,
+                                 2,2,0,2,2,
+                                 2,2,2,2,2,
+                                 2,2,2,2,2};
+
+  cv::Mat1f inverse_depth_prior = cv::Mat1f(5,5,data_inverse_depth_prior);
+  cv::Mat1f variance_prior = cv::Mat1f(5,5,data_variance_prior);
+  cv::Mat1f inverse_depth_obs = cv::Mat1f(5,5,data_inverse_depth_obs);
+  cv::Mat1f variance_obs = cv::Mat1f(5,5,data_variance2_obs) ;
+
+  depth_ma_fusionner fusion(inverse_depth_obs,variance_obs,inverse_depth_prior,variance_prior);
+  cv::Mat1f posterior = fusion.get_inverse_depth_posterior();
+  cv::Mat1f variance = fusion.get_inverse_depth_posterior_variance();
+
+  for (int x = 0; x < 5; ++x) {
+    for (int y = 0; y < 5; ++y) {
+      if(x==2 && y==2){
+        EXPECT_EQ(posterior(x,y),0);
+        EXPECT_EQ(variance(x,y),0);
+      }
+      else{
+        EXPECT_EQ(posterior(x,y),1);
+        EXPECT_EQ(variance(x,y),1);
+      }
+    }
+  }
+}
+
+TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_obs_variance_no_prior_data)
+{
+  float data_inverse_depth_prior[25] = {1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,0,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1};
+  float data_variance_prior[25] = {2,2,2,2,2,
+                                  2,2,2,2,2,
+                                  2,2,2,2,2,
+                                  2,2,2,2,2,
+                                  2,2,2,2,2};
+  float data_inverse_depth_obs[25] = {1,1,1,1,1,
+                                     1,1,1,1,1,
+                                     1,1,1,1,1,
+                                     1,1,1,1,1,
+                                     1,1,1,1,1};
+  float data_variance2_obs[25] = {2,2,2,2,2,
+                                 2,2,2,2,2,
+                                 2,2,0,2,2,
+                                 2,2,2,2,2,
+                                 2,2,2,2,2};
+
+  cv::Mat1f inverse_depth_prior = cv::Mat1f(5,5,data_inverse_depth_prior);
+  cv::Mat1f variance_prior = cv::Mat1f(5,5,data_variance_prior);
+  cv::Mat1f inverse_depth_obs = cv::Mat1f(5,5,data_inverse_depth_obs);
+  cv::Mat1f variance_obs = cv::Mat1f(5,5,data_variance2_obs) ;
+
+  depth_ma_fusionner fusion(inverse_depth_obs,variance_obs,inverse_depth_prior,variance_prior);
+  cv::Mat1f posterior = fusion.get_inverse_depth_posterior();
+  cv::Mat1f variance = fusion.get_inverse_depth_posterior_variance();
+
+  for (int x = 0; x < 5; ++x) {
+    for (int y = 0; y < 5; ++y) {
+      if(x==2 && y==2){
+        EXPECT_EQ(posterior(x,y),0);
+        EXPECT_EQ(variance(x,y),0);
+      }
+      else{
+        EXPECT_EQ(posterior(x,y),1);
+        EXPECT_EQ(variance(x,y),1);
+      }
+    }
+  }
+}
+
+TEST(depth_map_fusionner_test, depth_map_fusionner_test_no_obs_data_no_prior_variance)
+{
+  float data_inverse_depth_prior[25] = {1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1,
+                                       1,1,1,1,1};
+  float data_variance_prior[25] = {2,2,2,2,2,
+                                  2,2,2,2,2,
+                                  2,2,0,2,2,
+                                  2,2,2,2,2,
+                                  2,2,2,2,2};
+  float data_inverse_depth_obs[25] = {1,1,1,1,1,
+                                     1,1,1,1,1,
+                                     1,1,0,1,1,
+                                     1,1,1,1,1,
+                                     1,1,1,1,1};
+  float data_variance2_obs[25] = {2,2,2,2,2,
+                                 2,2,2,2,2,
+                                 2,2,2,2,2,
+                                 2,2,2,2,2,
+                                 2,2,2,2,2};
+
+  cv::Mat1f inverse_depth_prior = cv::Mat1f(5,5,data_inverse_depth_prior);
+  cv::Mat1f variance_prior = cv::Mat1f(5,5,data_variance_prior);
+  cv::Mat1f inverse_depth_obs = cv::Mat1f(5,5,data_inverse_depth_obs);
+  cv::Mat1f variance_obs = cv::Mat1f(5,5,data_variance2_obs) ;
+
+  depth_ma_fusionner fusion(inverse_depth_obs,variance_obs,inverse_depth_prior,variance_prior);
+  cv::Mat1f posterior = fusion.get_inverse_depth_posterior();
+  cv::Mat1f variance = fusion.get_inverse_depth_posterior_variance();
+
+  for (int x = 0; x < 5; ++x) {
+    for (int y = 0; y < 5; ++y) {
+      if(x==2 && y==2){
+        EXPECT_EQ(posterior(x,y),0);
+        EXPECT_EQ(variance(x,y),0);
+      }
+      else{
+        EXPECT_EQ(posterior(x,y),1);
+        EXPECT_EQ(variance(x,y),1);
+      }
+    }
+  }
+}
+
 } //sdvo
 int main(int argc, char *argv[])
 {
